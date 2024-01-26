ff433f642530e66912b5ac221effed2968e7a965
Dmitriy Pertsev <davaeron@gmail.com>
2024-01-26 18:33:59 +0300

    upd

--- mix.exs
+++ mix.exs
@@ -5,21 +5,23 @@ defmodule NetAddress.MixProject do
     [
       app: :net_address,
       version: "0.3.0",
-      elixir: "~> 1.10",
+      elixir: "~> 1.16",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       test_coverage: [tool: ExCoveralls],
-      preferred_cli_env:
-        [coveralls: :test,
-         "coveralls.detail": :test,
-         "coveralls.post": :test,
-         "coveralls.html": :test],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test,
+        "coveralls.cobertura": :test
+      ],
       package: [
-          description: "IP address and Mac address tools",
-          licenses: ["MIT"],
-          files: ~w(lib mix.exs README* LICENSE* VERSIONS*),
-          links: %{"GitHub" => "https://github.com/ityonemo/net_address"}
-        ],
+        description: "IP address and Mac address tools",
+        licenses: ["MIT"],
+        files: ~w(lib mix.exs README* LICENSE* VERSIONS*),
+        links: %{"GitHub" => "https://github.com/ityonemo/net_address"}
+      ],
       docs: [
         source_url: "https://github.com/ityonemo/net_address"
       ]
@@ -30,13 +32,18 @@ defmodule NetAddress.MixProject do
     [extra_applications: [:logger]]
   end
 
-  defp deps, do: [
-    {:credo, "~> 1.3", only: [:dev, :test], runtime: false},
-    {:excoveralls, "~> 0.11", only: :test, runtime: false},
-    {:ex_doc, "> 0.0.0", only: :dev, runtime: false},
-    {:dialyxir, "~> 0.5.1", only: :dev, runtime: false},
-    # for testing ip.random
-    {:stream_data, "~> 0.5.0", only: :test}
-  ]
+  defp deps,
+    do: [
+      # static analysis tools
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false},
+      {:styler, "~> 0.11", only: [:dev, :test], runtime: false},
 
+      # docs dependencies
+      {:ex_doc, "~> 0.31", only: :dev, runtime: false},
+
+      # testing
+      {:excoveralls, "~> 0.18", only: :test},
+      {:stream_data, "~> 0.5", only: :test}
+    ]
 end
